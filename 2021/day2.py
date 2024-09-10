2bd26dcb4964f1027f49f6ae2bc0c886f141a8da
olliejth <ojthompson.64@gmail.com>
2024-09-10 14:57:02 +0100

    Day 2 2021 AoC finished.

--- 2021/day2.py
+++ 2021/day2.py
@@ -2,25 +2,28 @@
 def get_new_positions(direction: list, position: dict[int]) -> tuple[int, int]:
     h_position = position["horizontal"]
     depth = position["depth"]
+    aim = position["aim"]
 
     if direction[0] == "forward":
         h_position += int(direction[1])
+        depth += (int(direction[1]) * aim)
     elif direction[0] == "up":
-        depth -= int(direction[1])
+        aim -= int(direction[1])
     elif direction[0] == "down":
-        depth += int(direction[1])
+        aim += int(direction[1])
 
-    return h_position, depth
+    return h_position, depth, aim
 
 
 def get_final_positions(directions: list[str]) -> int:
-    positions = {"horizontal": 0, "depth": 0}
+    positions = {"horizontal": 0, "depth": 0, "aim": 0}
 
     for direction in directions:
         dir_list = direction.split()
-        new_h, new_d = get_new_positions(dir_list, positions)
+        new_h, new_d, new_aim = get_new_positions(dir_list, positions)
         positions["horizontal"] = new_h
         positions["depth"] = new_d
+        positions["aim"] = new_aim
 
     res = positions["horizontal"] * positions["depth"]
 
