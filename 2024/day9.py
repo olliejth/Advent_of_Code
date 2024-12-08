fc672d4e89d535f37789726aed2d1e44c196ef09
olliejth <ojthompson.64@gmail.com>
2024-12-08 12:34:06 +0000

    Day 8 done.

---
+++ 2024/day9.py
@@ -0,0 +1,12 @@
+"""AoC day 9 2024"""
+
+
+def get_result(data: list):
+    '''Master function'''
+    pass
+
+
+if __name__ == "__main__":
+
+    with open('data.txt', 'r', encoding='utf-8') as f_obj:
+        data = [obj.strip() for obj in f_obj.readlines()]
